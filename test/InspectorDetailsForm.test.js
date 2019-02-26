b1d961ca903ca043e577a7e936dff3701ecca1b1
Carsten Wickner <Carsten.Wickner@gmx.de>
2019-02-26 21:30:17 +0100

    add unit tests for InspectorDetailsForm

---
+++ test/InspectorDetailsForm.test.js
@@ -0,0 +1,64 @@
+import React from "react";
+import { shallow } from "enzyme";
+import InspectorDetailsForm from "../src/InspectorDetailsForm";
+
+describe("renders correctly", () => {
+    it("with minimal/default props", () => {
+        const component = shallow(
+            <InspectorDetailsForm
+                fields={[
+                    {
+                        labelText: "Field One",
+                        rowValue: 1
+                    },
+                    {
+                        labelText: "Field Two",
+                        rowValue: "text"
+                    }
+                ]}
+            />
+        );
+        expect(component).toMatchSnapshot();
+    });
+    it("ignoring undefined/null rowValues", () => {
+        const component = shallow(
+            <InspectorDetailsForm
+                fields={[
+                    {
+                        labelText: "Field One"
+                    },
+                    {
+                        labelText: "Field Two",
+                        rowValue: undefined
+                    },
+                    {
+                        labelText: "Field Three",
+                        rowValue: null
+                    }
+                ]}
+            />
+        );
+        expect(component.find(".jsonschema-inspector-details-form-row").length).toBe(0);
+    });
+    it("including other falsy rowValues", () => {
+        const component = shallow(
+            <InspectorDetailsForm
+                fields={[
+                    {
+                        labelText: "Field One",
+                        rowValue: 0
+                    },
+                    {
+                        labelText: "Field Two",
+                        rowValue: false
+                    },
+                    {
+                        labelText: "Field Three",
+                        rowValue: ""
+                    }
+                ]}
+            />
+        );
+        expect(component.find(".jsonschema-inspector-details-form-row").length).toBe(3);
+    });
+});
