d633a0bf2da455eee372c812d5f29b8d9d415308
Carsten Wickner <Carsten.Wickner@gmx.de>
2019-10-09 21:46:17 +0200

    Only produce single ES module as output

--- rollup.config.js
+++ rollup.config.js
@@ -4,89 +4,35 @@ import external from "rollup-plugin-peer-deps-external";
 import postcss from "rollup-plugin-postcss";
 import resolve from "rollup-plugin-node-resolve";
 import { terser } from "rollup-plugin-terser";
-import { uglify } from "rollup-plugin-uglify";
 
 import packageJSON from "./package.json";
 
-const input = "./src/index.js";
-const babelPluginOptions = {
-    exclude: "node_modules/**"
-};
-const postcssPluginOptions = {
-    extract: false,
-    extensions: [".scss"]
-};
-const minifyExtension = pathToFile => pathToFile.replace(/\.js$/, ".min.js");
-
 export default [
-    // CommonJS
     {
-        input,
+        input: "./src/index.js",
         output: {
             file: packageJSON.main,
-            format: "cjs",
-            sourcemap: true
-        },
-        plugins: [
-            external(),
-            postcss(postcssPluginOptions),
-            babel(babelPluginOptions),
-            resolve(),
-            commonjs()
-        ]
-    },
-    // CommonJS (min)
-    {
-        input,
-        output: {
-            file: minifyExtension(packageJSON.main),
-            format: "cjs"
-        },
-        plugins: [
-            external(),
-            postcss({
-                ...postcssPluginOptions,
-                minimize: true
-            }),
-            babel(babelPluginOptions),
-            resolve(),
-            commonjs(),
-            uglify()
-        ]
-    },
-    // ES Module
-    {
-        input,
-        output: {
-            file: packageJSON.module,
             format: "es",
             exports: "named"
         },
         plugins: [
+            // ensure peer dependencies are declared as externals
             external(),
-            postcss(postcssPluginOptions),
-            babel(babelPluginOptions),
-            resolve(),
-            commonjs()
-        ]
-    },
-    // ES Module (min)
-    {
-        input,
-        output: {
-            file: minifyExtension(packageJSON.module),
-            format: "es",
-            exports: "named"
-        },
-        plugins: [
-            external(),
+            // collect styles from SCSS, minimise them and include them in the JS module (i.e. not as separate .css file)
             postcss({
-                ...postcssPluginOptions,
+                extract: false,
+                extensions: [".scss"],
                 minimize: true
             }),
-            babel(babelPluginOptions),
+            // transpile for compatibility
+            babel({
+                exclude: "node_modules/**"
+            }),
+            // resolve dependencies that are ES modules
             resolve(),
+            // resolve dependencies that are (legacy) CommonJS modules
             commonjs(),
+            // discard unused parts of the code
             terser()
         ]
     }
