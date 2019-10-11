4d3b134a1ebcfee540360b234bd2ad54c2d35beb
Carsten Wickner <11309681+CarstenWickner@users.noreply.github.com>
2019-10-11 23:36:58 +0200

    Fix rollup build (#18)
    * Declare ES "module" in package.json
    * Provide CommonJS output as fall-back
    * Declare all dependencies as external to allow tree-shaking by consumers

--- rollup.config.js
+++ rollup.config.js
@@ -1,33 +1,64 @@
 import babel from "rollup-plugin-babel";
 import commonjs from "rollup-plugin-commonjs";
-import external from "rollup-plugin-peer-deps-external";
 import postcss from "rollup-plugin-postcss";
 import resolve from "rollup-plugin-node-resolve";
 import { terser } from "rollup-plugin-terser";
+import { uglify } from "rollup-plugin-uglify";
 
 import packageJSON from "./package.json";
 
+const input = "./src/index.js";
+const external = [
+    // ensure peer dependencies are declared as externals
+    ...Object.keys(packageJSON.peerDependencies),
+    // same for actual dependencies, to allow for dependency resolution/de-duplication by consumers
+    ...Object.keys(packageJSON.dependencies)
+];
+const postcssOptions = {
+    extract: false,
+    extensions: [".scss"],
+    minimize: true
+};
+const babelOptions = {
+    exclude: "node_modules/**"
+};
+
 export default [
+    // CommonJS
     {
-        input: "./src/index.js",
+        input,
         output: {
             file: packageJSON.main,
+            format: "cjs"
+        },
+        external,
+        plugins: [
+            // collect styles from SCSS, minimise them and include them in the JS module (i.e. not as separate .css file)
+            postcss(postcssOptions),
+            // transpile for compatibility
+            babel(babelOptions),
+            // resolve dependencies that are ES modules
+            resolve(),
+            // resolve dependencies that are (legacy) CommonJS modules
+            commonjs(),
+            // minify to reduce size
+            uglify()
+        ]
+    },
+    // ES Module
+    {
+        input,
+        output: {
+            file: packageJSON.module,
             format: "es",
             exports: "named"
         },
+        external,
         plugins: [
-            // ensure peer dependencies are declared as externals
-            external(),
             // collect styles from SCSS, minimise them and include them in the JS module (i.e. not as separate .css file)
-            postcss({
-                extract: false,
-                extensions: [".scss"],
-                minimize: true
-            }),
+            postcss(postcssOptions),
             // transpile for compatibility
-            babel({
-                exclude: "node_modules/**"
-            }),
+            babel(babelOptions),
             // resolve dependencies that are ES modules
             resolve(),
             // resolve dependencies that are (legacy) CommonJS modules
