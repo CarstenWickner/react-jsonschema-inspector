2c90241acfc2535602760b0127c68b105fac5167
Carsten Wickner <Carsten.Wickner@gmx.de>
2019-10-30 21:24:30 +0100

    Use @wessberg/rollup-plugin-ts
    Getting rid of four separate plugins instead
    Also allowing same configuration for CJS and ES bundles

--- rollup.config.js
+++ rollup.config.js
@@ -1,78 +1,41 @@
-import commonjs from "rollup-plugin-commonjs";
 import postcss from "rollup-plugin-postcss";
-import resolve from "rollup-plugin-node-resolve";
 import { terser } from "rollup-plugin-terser";
-import typescript from "rollup-plugin-typescript2";
-import { uglify } from "rollup-plugin-uglify";
+import typescript from "@wessberg/rollup-plugin-ts";
 
 import packageJSON from "./package.json";
-
-const input = "./src/index.ts";
-const external = [
-    // ensure peer dependencies are declared as externals
-    ...Object.keys(packageJSON.peerDependencies),
-    // same for actual dependencies, to allow for dependency resolution/de-duplication by consumers
-    ...Object.keys(packageJSON.dependencies)
-];
-const postcssOptions = {
-    extract: false,
-    extensions: [".scss"],
-    minimize: true
-};
-const typescriptOptions = {
-    // eslint-disable-next-line global-require
-    typescript: require("typescript")
-};
 
 export default [
     // CommonJS
     {
-        input,
-        output: {
-            file: packageJSON.main,
-            format: "cjs",
-            sourcemap: true
-        },
-        external,
+        input: "./src/index.ts",
+        output: [
+            {
+                file: packageJSON.main,
+                format: "cjs",
+                sourcemap: true
+            },
+            {
+                file: packageJSON.module,
+                format: "es",
+                exports: "named",
+                sourcemap: true
+            }
+        ],
+        external: [
+            // ensure peer dependencies are declared as externals
+            ...Object.keys(packageJSON.peerDependencies),
+            // same for actual dependencies, to allow for dependency resolution/de-duplication by consumers
+            ...Object.keys(packageJSON.dependencies)
+        ],
         plugins: [
             // collect styles from SCSS, minimise them and include them in the JS module (i.e. not as separate .css file)
-            postcss(postcssOptions),
-            // compile typescript into vanilla javascript
-            typescript({
-                ...typescriptOptions,
-                tsconfigOverride: {
-                    compilerOptions: {
-                        target: "es5"
-                    }
-                }
+            postcss({
+                extract: false,
+                extensions: [".scss"],
+                minimize: true
             }),
-            // resolve dependencies that are ES modules
-            resolve(),
-            // resolve dependencies that are (legacy) CommonJS modules
-            commonjs(),
-            // minify to reduce size
-            uglify()
-        ]
-    },
-    // ES Module
-    {
-        input,
-        output: {
-            file: packageJSON.module,
-            format: "es",
-            exports: "named",
-            sourcemap: true
-        },
-        external,
-        plugins: [
-            // collect styles from SCSS, minimise them and include them in the JS module (i.e. not as separate .css file)
-            postcss(postcssOptions),
-            // compile typescript into vanilla javascript
-            typescript(typescriptOptions),
-            // resolve dependencies that are ES modules
-            resolve(),
-            // resolve dependencies that are (legacy) CommonJS modules
-            commonjs(),
+            // compile typescript into vanilla javascript and produce a single .d.ts file
+            typescript({}),
             // discard unused parts of the code
             terser()
         ]
