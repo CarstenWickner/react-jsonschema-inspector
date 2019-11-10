cf7493134a41a623e5d6ab1f42964f69ab1a23f5
Carsten Wickner <11309681+CarstenWickner@users.noreply.github.com>
2019-11-10 13:29:10 +0100

    chore(package): update prettier to version 1.19.1 (#41)
    Closes #40

--- src/types/typeUtils.ts
+++ src/types/typeUtils.ts
@@ -5,7 +5,9 @@
 export type KnownKeys<T> = {
     [K in keyof T]: string extends K ? never : number extends K ? never : K;
 } extends { [_ in keyof T]: infer U }
-    ? ({} extends U ? never : U)
+    ? {} extends U
+        ? never
+        : U
     : never;
 
 /**
